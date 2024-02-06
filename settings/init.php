ec2ea05e4d624d8bfe38d45d3d8f1ec977b328bf
Kim <kim@flex4b.com>
2024-02-06 19:32:09 +0100

    U: Sæt error report på ved "rigtige fejl"

--- settings/init.php
+++ settings/init.php
@@ -1,5 +1,9 @@
 <?php
 require "classes/classDB.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
+
 
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
