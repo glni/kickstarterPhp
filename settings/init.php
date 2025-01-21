2e920d80fa8b8f904118987c701d988c20e65115
Glen <glen@flex4b.com>
2025-01-21 14:49:27 +0100

    Update comments to remove CONFIG_LIVE Docker configuration
    Simplified the CONFIG_LIVE documentation by removing references to the Docker-specific configuration option. Adjusted usage instructions to reflect the updated setup, ensuring clarity and streamlining the configuration process.

--- settings/init.php
+++ settings/init.php
@@ -5,22 +5,17 @@ ini_set('display_startup_errors', 1);
 
 /*
  * Guide
- * define("CONFIG_LIVE", "0"); kan skiftes til f.eks. define("CONFIG_LIVE", "1"); for at ændre til live eller define("CONFIG_LIVE", "2"); til docker
+ * define("CONFIG_LIVE", "0"); kan skiftes til f.eks. define("CONFIG_LIVE", "1"); for at ændre til live
  *
  * CONFIG_LIVE == 0
- * I den første del af if sætningen er databasen defineret for local brug på din computer med WAMP eller MAMP
- * Her er standard brugernavn root og password ingenting. MAC skal som regel ændre password ($DB_PASS) til også at være root
+ * I den første del af if-sætningen er databasen defineret for local brug på din computer med docker
+ * Her er standard brugernavn user og adgangskoden er password.
  *
  * CONFIG_LIVE == 1
  * Her sætter du dine oplysninger til webhotellet f.eks. Simply.com
  * Inden du uploader denne fil til webhotellet retter du define("CONFIG_LIVE", "0"); til define("CONFIG_LIVE", "1"); så den vælger de korrekte oplysninger
- * Når filen er uploadet retter du igen til define("CONFIG_LIVE", "0"); ved WAMP/MAMP eller define("CONFIG_LIVE", "2"); ved docker så det virker lokalt igen
+ * Når filen er uploadet retter du igen til define("CONFIG_LIVE", "0"); så det virker lokalt igen
  * På den måde kan du både have lokale oplysninger og webhotel oplysninger liggende
- *
- * CONFIG_LIVE == 2
- * Denne er lavet så man kan bruge Docker
- * Her er sat standard brugernavn og password som kan ændres i _docker/.env (det skal kun rettes hvis du ikke vil bruge standard)
- * I Docker er der til at starte med lavet en standard database: webshop med en tabel: produkter som benyttes i undervisningen og gør at index.php virker
  *
  * GENERELT
  * $DB_NAME skal rettes til det din database for dette projekt hedder. Databasen er den du opretter i PHPMyAdmin
