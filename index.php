810f12166715c06787b1fe2f2a25748ed21b2002
Glen <glen@flex4b.com>
2025-01-21 08:40:09 +0100

    Use CDN for Bootstrap JS instead of local dependency
    Replaced the local Bootstrap JavaScript file with a CDN link for version 5.3.3. This reduces reliance on local dependencies and ensures the latest minor updates and optimizations.

--- index.php
+++ index.php
@@ -20,6 +20,6 @@ require "settings/init.php";
 <body>
 
 
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
